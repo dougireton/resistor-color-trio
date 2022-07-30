245ec2966a38cd4d15777bdafb8078ee76e00e5b
Doug Ireton <doug@allma.io>
2022-07-30 15:55:54 -0700

    Finish solution

--- resistor-color-trio.ts
+++ resistor-color-trio.ts
@@ -1,46 +1,49 @@
 class Resistor {
-  readonly firstBand: number;
-  readonly secondBand: number;
-  readonly thirdBand: number;
+  private readonly firstBand: string;
+  private readonly secondBand: string;
+  private readonly thirdBand: string;
   readonly ohms: number;
 
-  private readonly resistanceValues: { [key: string]: number } = Object.freeze({
-    "black": 0,
-    "brown": 1,
-    "red": 2,
-    "orange": 3,
-    "yellow": 4,
-    "green": 5,
-    "blue": 6,
-    "violet": 7,
-    "grey": 8,
-    "white": 9,
-  });
-
   constructor(resistorBands: string[]) {
-    this.firstBand = this.resistanceValues[resistorBands[0]];
-    this.secondBand = this.resistanceValues[resistorBands[1]];
-    this.thirdBand = this.multiplier(resistorBands[2]);
+    this.firstBand = resistorBands[0];
+    this.secondBand = resistorBands[1];
+    this.thirdBand = resistorBands[2];
     this.ohms = this.getOhms();
-  }
-
-  private multiplier(color: string): number {
-    return this.resistanceValues[color] * 10
   }
 
   /**
    * getOhms
  : number  */
   private getOhms(): number {
-    return ((this.firstBand * 10) + (this.secondBand)) * this.thirdBand
+    const resistanceValues: { [key: string]: number } = Object.freeze({
+      "black": 0,
+      "brown": 1,
+      "red": 2,
+      "orange": 3,
+      "yellow": 4,
+      "green": 5,
+      "blue": 6,
+      "violet": 7,
+      "grey": 8,
+      "white": 9,
+    });
+
+    const firstValue = resistanceValues[this.firstBand];
+    const secondValue = resistanceValues[this.secondBand];
+    const multiplier = 10 ** resistanceValues[this.thirdBand]
+
+    return ((firstValue * 10) + (secondValue)) * multiplier
   }
 }
-
 
 export function decodedResistorValue(resistorBands: string[]): string {
 
   const myResistor = new Resistor(resistorBands);
 
-  return `${myResistor.ohms} ohms`;
+  if (myResistor.ohms < 1000) {
+    return `${myResistor.ohms} ohms`;
+  }
 
+  const kiloohms = myResistor.ohms / 1000
+  return `${kiloohms} kiloohms`
 }
